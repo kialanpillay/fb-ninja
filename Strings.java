1288a92411b24df260ab0141511d6d66ce68660d
Kialan Pillay <kialanpillay@gmail.com>
2020-07-17 20:26:39 +0200

    str2int

---
+++ Strings.java
@@ -0,0 +1,9 @@
+public class Strings{
+
+    static int str2int(String s){
+        return Integer.parseInt(s);
+    }
+    public static void main(String[]args){
+        System.out.println(str2int("-30"));
+    }
+}
