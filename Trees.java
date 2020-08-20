72e0a0367e6127cf763f064c37a667826ceacf90
Kialan Pillay <kialanpillay@gmail.com>
2020-08-20 14:10:08 +0200

    equality

--- Trees.java
+++ Trees.java
@@ -16,6 +16,29 @@ public class Trees{
         return false;
     }
 
+    static void insert(Node root, HashSet<Integer> s){
+        insert(root.left, s); //Call on left-subtree
+        s.add(root.data); //Insert data into set
+        insert(root.right, s); //Call on right-subtree
+    }
+
+    //Assuming the trees consist of Integer
+    static boolean equality(Node root1, Node root2){
+        //Check if each root node is null, return true if both are, else false
+        if(root1 == null){
+          return (root2 == null);
+        }
+
+        // Instantiate sets, and convert trees
+        Set<Integer> s1 = new HashSet<Integer>();
+        Set<Integer> s2 = new HashSet<Integer>();
+        insert(root1, s1);
+        insert(root2, s2);
+
+        // Return true if both sets are equal
+        return s1.equals(s2);
+    }
+
     public static void main(String[]args){
 
     }
